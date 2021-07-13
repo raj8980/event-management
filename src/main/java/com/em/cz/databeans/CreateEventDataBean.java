6b32322e3bf8d060b37708bbd093fd1bf20fc01a
Deepa101001 <69865357+Deepa101001@users.noreply.github.com>
2021-07-13 22:43:12 +0530

    Create CreateEventDataBean.java

---
+++ src/main/java/com/em/cz/databeans/CreateEventDataBean.java
@@ -0,0 +1,102 @@
+package com.em.cz.databeans;
+public class createEventDataBean {  
+private String eventType;
+	private String eventTitle;
+	private String eventDesciption;
+	private String startDate;
+	private String endDate;
+	private String startTime;
+	private String endTime;
+	private String accessType;
+	private String requireParticipantFees;
+	private String participantFees;
+	private String participantType;
+	private String requirePreCondi;
+	private String preConditionType;
+	private String preConditionDtls;
+	public String getEventType() {
+		return eventType;
+	}
+	public void setEventType(String eventType) {
+		this.eventType = eventType;
+	}
+	public String getEventTitle() {
+		return eventTitle;
+	}
+	public void setEventTitle(String eventTitle) {
+		this.eventTitle = eventTitle;
+	}
+	public String getEventDesciption() {
+		return eventDesciption;
+	}
+	public void setEventDesciption(String eventDesciption) {
+		this.eventDesciption = eventDesciption;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	public String getAccessType() {
+		return accessType;
+	}
+	public void setAccessType(String accessType) {
+		this.accessType = accessType;
+	}
+	public String getRequireParticipantFees() {
+		return requireParticipantFees;
+	}
+	public void setRequireParticipantFees(String requireParticipantFees) {
+		this.requireParticipantFees = requireParticipantFees;
+	}
+	public String getParticipantFees() {
+		return participantFees;
+	}
+	public void setParticipantFees(String participantFees) {
+		this.participantFees = participantFees;
+	}
+	public String getParticipantType() {
+		return participantType;
+	}
+	public void setParticipantType(String participantType) {
+		this.participantType = participantType;
+	}
+	public String getRequirePreCondi() {
+		return requirePreCondi;
+	}
+	public void setRequirePreCondi(String requirePreCondi) {
+		this.requirePreCondi = requirePreCondi;
+	}
+	public String getPreConditionType() {
+		return preConditionType;
+	}
+	public void setPreConditionType(String preConditionType) {
+		this.preConditionType = preConditionType;
+	}
+	public String getPreConditionDtls() {
+		return preConditionDtls;
+	}
+	public void setPreConditionDtls(String preConditionDtls) {
+		this.preConditionDtls = preConditionDtls;
+	}
+	
+}
